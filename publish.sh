0e7ff45d7bc735e34b19852b838be38e68e16c02
Manav Rathi <manav@mnvr.in>
2025-05-30 20:15:07 +0530

    publish

--- publish.sh
+++ publish.sh
@@ -3,8 +3,18 @@
 # Convert Jupyter notebooks into HTML pages, tack on an index, and
 # then push to the gh-pages branch on remote.
 
-# git checkout gh-pages
+set -o xtrace
+set -o errexit
+
+git checkout -b gh-pages
 mkdir -p docs
 
 jupyter nbconvert --to html --template classic --output-dir=docs *.ipynb
 sed "s#<time></time>#<time>`date '+%b %d, %Y'`</time>#" index.html >docs/index.html
+
+git add docs
+git commit -m render
+git push origin gh-pages
+git checkout main
+git push
+
